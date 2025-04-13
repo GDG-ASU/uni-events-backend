98fd8436ebca67fd5bf9743e610329537064cb9f
FirasHani <77682961+FirasHani@users.noreply.github.com>
2025-04-13 22:57:33 +0300

    Create main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,20 @@
-package server
+package main
+
+import (
+	"net/http"
+	"uni-events-backend/config"
+
+	// "uni-events-backend/internal/api"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+    config.InitDB()
+    e := echo.New()
+	e.GET("/",func(c echo.Context)error{
+		return c.String(http.StatusOK,"hel")
+	})
+    // api.RegisterRoutes(e)
+    e.Logger.Fatal(e.Start(":8080"))
+}
