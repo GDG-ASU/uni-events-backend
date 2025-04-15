8717e1d1ebcb7954f6c1527068fe55248b8a107b
FirasHani <77682961+FirasHani@users.noreply.github.com>
2025-04-15 23:18:46 +0300

    Update main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
-	"net/http"
 	"uni-events-backend/config"
-
-	// "uni-events-backend/internal/api"
+	"uni-events-backend/internal/api/user"
+	"uni-events-backend/internal/middlewares"
+	"uni-events-backend/internal/repositories"
+	"uni-events-backend/internal/service"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
-    config.InitDB()
-    e := echo.New()
-	e.GET("/",func(c echo.Context)error{
-		return c.String(http.StatusOK,"hel")
-	})
-    // api.RegisterRoutes(e)
-    e.Logger.Fatal(e.Start(":8080"))
+	e := echo.New()
+	db := config.InitDB()
+	
+	// db.AutoMigrate(&models.User{}) 
+	
+	userRepo := repositories.NewUserRepository(db)
+	userService := service.NewUserService(userRepo)
+	userHandler := user.NewHandler(userService)
+
+	// Group routes
+	apiGroup := e.Group("/api/v1")
+	userGroup := apiGroup.Group("/users")
+	userGroup.Use(middlewares.ClerkAuthMiddleware)
+	userGroup.GET("/me", userHandler.Me)
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
