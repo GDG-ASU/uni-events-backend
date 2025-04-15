1caa9f01a6a3fd8750e901e3e846a868b2a6c962
FirasHani <77682961+FirasHani@users.noreply.github.com>
2025-04-15 23:20:11 +0300

    Create handler for user

---
+++ internal/api/user/handler.go
@@ -0,0 +1,42 @@
+package user
+
+import (
+	"net/http"
+	"uni-events-backend/internal/models"
+	"uni-events-backend/pkg/utils"
+
+	"github.com/labstack/echo/v4"
+
+	"uni-events-backend/internal/service"
+)
+
+type Handler struct {
+	service service.UserService
+}
+
+func NewHandler(userService service.UserService) *Handler {
+	return &Handler{
+		service: userService,
+	}
+}
+
+func (h *Handler) Me(c echo.Context) error {
+	clerkID := utils.GetClerkUserID(c)
+
+	user, err := h.service.GetUserByClerkID(c.Request().Context(),clerkID)
+	if err != nil {
+		newUser := &models.User{
+			ClerkID: clerkID,
+			Email:   c.Get("email").(string),
+			Role:    "student",
+		}
+		createdUser, err := h.service.CreateUserIfNotExists(c.Request().Context(), newUser)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, echo.Map{"error": err.Error()})
+		}
+		
+		return c.JSON(http.StatusOK, createdUser)
+	}
+
+	return c.JSON(http.StatusOK, user)
+}
