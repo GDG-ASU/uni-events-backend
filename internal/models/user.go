e3f06130aaa05319f605d91cad53269f9abc015c
FirasHani <77682961+FirasHani@users.noreply.github.com>
2025-04-15 23:20:44 +0300

    Create user model

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,1 +1,11 @@
 package models
+
+import "gorm.io/gorm"
+
+type User struct {
+    gorm.Model
+    ClerkID string `gorm:"uniqueIndex" json:"clerk_id"`
+    Name    string `json:"name"`
+    Email   string `gorm:"uniqueIndex" json:"email"`
+    Role    string `json:"role"`
+}
