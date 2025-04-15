b954196e72a2bee974fb6a16701c0f7c0ef10d82
FirasHani <77682961+FirasHani@users.noreply.github.com>
2025-04-15 23:21:44 +0300

    Create token utils

--- pkg/utils/token.go
+++ pkg/utils/token.go
@@ -1,1 +1,10 @@
 package utils
+
+import "github.com/labstack/echo/v4"
+
+func GetClerkUserID(c echo.Context) string {
+    if userID, ok := c.Get("user_id").(string); ok {
+        return userID
+    }
+    return ""
+}
