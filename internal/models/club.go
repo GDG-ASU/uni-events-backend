19b1711e62e253f854448ccba821b329f76a7bbb
FirasHani <77682961+FirasHani@users.noreply.github.com>
2025-04-18 22:10:23 +0300

    Add Club modle

--- internal/models/club.go
+++ internal/models/club.go
@@ -1,1 +1,10 @@
 package models
+
+import "gorm.io/gorm"
+
+type Club struct {
+	gorm.Model
+	Name        string       `json:"name"`
+	Description string       `json:"description"`
+	Owners      []ClubOwner  `gorm:"foreignKey:ClubID" json:"owners"`
+}
