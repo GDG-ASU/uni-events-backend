eabd254d3f241ab23892b52c104c3c80b1d53a82
FirasHani <77682961+FirasHani@users.noreply.github.com>
2025-04-18 22:11:33 +0300

    Add club service logic

--- internal/service/club_service.go
+++ internal/service/club_service.go
@@ -1,1 +1,24 @@
 package service
+
+import (
+	"context"
+	"uni-events-backend/internal/models"
+	"uni-events-backend/internal/repositories"
+)
+
+type ClubService interface {
+	CreateClub(ctx context.Context, club *models.Club) (*models.Club, error)
+}
+
+type clubService struct {
+	repo repositories.ClubRepository
+}
+
+func NewClubService(repo repositories.ClubRepository) ClubService {
+	return &clubService{repo}
+}
+
+func (s *clubService) CreateClub(ctx context.Context, club *models.Club) (*models.Club, error) {
+	err := s.repo.CreateClub(club)
+	return club, err
+}
