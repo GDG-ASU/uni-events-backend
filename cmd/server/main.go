1f00469fcf41479d63847084a4a8b7085753caed
FirasHani <77682961+FirasHani@users.noreply.github.com>
2025-04-18 23:54:57 +0300

    Update main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"uni-events-backend/config"
 	"uni-events-backend/internal/api/club"
+	"uni-events-backend/internal/api/event"
 	"uni-events-backend/internal/api/user"
 	"uni-events-backend/internal/middlewares"
 	"uni-events-backend/internal/models"
@@ -16,7 +17,7 @@ func main() {
 	e := echo.New()
 	db := config.InitDB()
 	
-   db.AutoMigrate(&models.User{},&models.Club{},&models.ClubOwner{}) 
+   db.AutoMigrate(&models.User{},&models.Club{},&models.ClubOwner{},&models.Event{}) 
 
 	userRepo := repositories.NewUserRepository(db)
 	userService := service.NewUserService(userRepo)
@@ -26,6 +27,10 @@ func main() {
 	clubRepo := repositories.NewClubRepository(db)
 	clubService := service.NewClubService(clubRepo)
 	clubHandler := club.NewHandler(clubService, userService)
+
+	eventRepo := repositories.NewEventRepository(db)
+	eventService := service.NewEventService(eventRepo)
+	eventHandler := event.NewHandler(clubService,userService,eventService)
 
 
 	// Group routes
@@ -39,6 +44,9 @@ func main() {
 	clubGroup.POST("/create-club",clubHandler.CreateClub)
 	clubGroup.PATCH("/clubs/:id", clubHandler.UpdateClub)
 
+	eventGroup := apiGroup.Group("/event")
+	eventGroup.Use(middlewares.ClerkAuthMiddleware)
+	eventGroup.POST("/create-event",eventHandler.CreateEvent)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
