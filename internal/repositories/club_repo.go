311e9e173febe0a14aaf4e75faca6707a2e5c6ad
FirasHani <77682961+FirasHani@users.noreply.github.com>
2025-04-18 22:11:17 +0300

    Add Club repo logic

--- internal/repositories/club_repo.go
+++ internal/repositories/club_repo.go
@@ -1,1 +1,23 @@
 package repositories
+
+import (
+	"uni-events-backend/internal/models"
+
+	"gorm.io/gorm"
+)
+
+type ClubRepository interface {
+	CreateClub(club *models.Club) error
+}
+
+type clubRepo struct {
+	db *gorm.DB
+}
+
+func NewClubRepository(db *gorm.DB) ClubRepository {
+	return &clubRepo{db}
+}
+
+func (r *clubRepo) CreateClub(club *models.Club) error {
+	return r.db.Create(club).Error
+}
