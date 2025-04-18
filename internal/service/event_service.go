e90d37d1ad8b27abbe70262b0147fd9675a20aa2
FirasHani <77682961+FirasHani@users.noreply.github.com>
2025-04-18 23:56:10 +0300

    Update event service

--- internal/service/event_service.go
+++ internal/service/event_service.go
@@ -1,1 +1,23 @@
 package service
+
+import (
+	"context"
+	"uni-events-backend/internal/models"
+	"uni-events-backend/internal/repositories"
+)
+
+type EventService interface {
+	CreateEvent(ctx context.Context, event *models.Event) (*models.Event, error)
+}
+
+type eventService struct {
+	repo repositories.EventRepository
+}
+
+func NewEventService(repo repositories.EventRepository) EventService {
+	return &eventService{repo}
+}
+
+func (s *eventService) CreateEvent(ctx context.Context, event *models.Event) (*models.Event, error) {
+	return s.repo.CreateEvent(ctx, event)
+}
