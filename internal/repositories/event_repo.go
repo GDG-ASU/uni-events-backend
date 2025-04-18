89b1085ae5cc9676946d85333c1105e87a2c214b
FirasHani <77682961+FirasHani@users.noreply.github.com>
2025-04-18 23:55:49 +0300

    Update event repo

--- internal/repositories/event_repo.go
+++ internal/repositories/event_repo.go
@@ -1,1 +1,27 @@
 package repositories
+
+import (
+	"context"
+	"uni-events-backend/internal/models"
+
+	"gorm.io/gorm"
+)
+
+type EventRepository interface {
+	CreateEvent(ctx context.Context, event *models.Event) (*models.Event, error)
+}
+
+type eventRepository struct {
+	db *gorm.DB
+}
+
+func NewEventRepository(db *gorm.DB) EventRepository {
+	return &eventRepository{db}
+}
+
+func (r *eventRepository) CreateEvent(ctx context.Context, event *models.Event) (*models.Event, error) {
+	if err := r.db.WithContext(ctx).Create(event).Error; err != nil {
+		return nil, err
+	}
+	return event, nil
+}
