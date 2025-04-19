e3b5e24b510aad40e3b66a957a3ad64229d99189
FirasHani <77682961+FirasHani@users.noreply.github.com>
2025-04-19 23:07:30 +0300

    Update event_repo.go
    Add event repo logic

--- internal/repositories/event_repo.go
+++ internal/repositories/event_repo.go
@@ -9,6 +9,10 @@ import (
 
 type EventRepository interface {
 	CreateEvent(ctx context.Context, event *models.Event) (*models.Event, error)
+	GetAllEvents(ctx context.Context) ([]*models.Event, error)
+	UpdateEvent(ctx context.Context, event *models.Event) (*models.Event, error)
+	DeleteEvent(ctx context.Context, id *models.Event) (*models.Event, error)
+	GetEventByID(ctx context.Context, id uint) (*models.Event, error)
 }
 
 type eventRepository struct {
@@ -24,4 +28,34 @@ func (r *eventRepository) CreateEvent(ctx context.Context, event *models.Event) 
 		return nil, err
 	}
 	return event, nil
+}
+
+func (r *eventRepository) GetAllEvents(ctx context.Context) ([]*models.Event, error) {
+	var events []*models.Event
+	if err := r.db.WithContext(ctx).Preload("Club").Find(&events).Error; err != nil {
+		return nil, err
+	}
+	return events, nil
+}
+
+func (r *eventRepository) GetEventByID(ctx context.Context, id uint) (*models.Event, error) {
+	var event models.Event
+	if err := r.db.WithContext(ctx).First(&event, id).Error; err != nil {
+		return nil, err
+	}
+	return &event, nil
+}
+
+func (r *eventRepository) UpdateEvent(ctx context.Context, event *models.Event) (*models.Event, error) {
+	if err := r.db.WithContext(ctx).Save(event).Error; err != nil {
+		return nil, err
+	}
+	return event, nil
+}
+
+func (r *eventRepository) DeleteEvent(ctx context.Context, event *models.Event) (*models.Event, error) {
+	if err := r.db.WithContext(ctx).Delete(event).Error; err != nil {
+		return nil,err
+	}
+	return event,nil
 }
